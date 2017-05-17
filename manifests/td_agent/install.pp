2a270993463b61f8f00eeb43ef0dfa69d06b3667
Israel Calvete <icalvete@gmail.com>
2017-05-17 18:23:09 +0200

    New plugin.

--- manifests/td_agent/install.pp
+++ manifests/td_agent/install.pp
@@ -4,8 +4,12 @@ class fluentd::td_agent::install {
     ensure => present,
   }
 
-  fluentd::plugin {$fluentd::params::plugins:
+  fluentd::plugin {'geoip-c':
     require => Package[$fluentd::params::package_td_agent]
+  }
+
+  fluentd::plugin {$fluentd::params::plugins:
+    require => [Package[$fluentd::params::package_td_agent], Fluentd::Plugin['geoip-c']]
   }
 }
 
